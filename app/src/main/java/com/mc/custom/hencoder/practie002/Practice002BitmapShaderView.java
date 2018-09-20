52f415f8030557d56dd1f6dd3a4d27fa1b4dda84
MilletCongee <zzzcy1014@aliyun.com>
2018-09-20 18:51:55 +0800

    batman_logo

--- app/src/main/java/com/mc/custom/hencoder/practie002/Practice002BitmapShaderView.java
+++ app/src/main/java/com/mc/custom/hencoder/practie002/Practice002BitmapShaderView.java
@@ -1,13 +1,21 @@
 package com.mc.custom.hencoder.practie002;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.mc.custom.R;
+
 /**
+ * BitmapShader 用 Bitmap 来着色
+ * 用 Bitmap 的像素来作为图形或文字的填
  * @author mc
  * @darte 2018/9/19
  * @email zzzcy1014@gmail.com
@@ -15,6 +23,8 @@ import android.view.View;
  */
 public class Practice002BitmapShaderView extends View {
     private Paint paint = new Paint();
+    private Paint paint1 = new Paint();
+    private Paint paint2 = new Paint();
     public Practice002BitmapShaderView(Context context) {
         super(context);
     }
@@ -26,6 +36,22 @@ public class Practice002BitmapShaderView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+        /**
+         * bitmap:用来做模板的 Bitmap 对象
+         * tileX:横向的 TileMode
+         * tileY:纵向的 TileMode
+         * */
+        Shader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        Shader shader1 = new BitmapShader(bitmap, Shader.TileMode.MIRROR, Shader.TileMode.MIRROR);
+        Shader shader2 = new BitmapShader(bitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
 
+        paint.setShader(shader);
+        paint1.setShader(shader1);
+        paint2.setShader(shader2);
+        //图形范围
+        //canvas.drawRect(0, 0, 800, 800, paint);
+        //canvas.drawRect(0, 0, 800, 800, paint);
+        canvas.drawRect(0, 0, 800, 800, paint2);
     }
 }
--- app/src/main/java/com/mc/custom/hencoder/practie002/Practice002ComposeShaderView.java
+++ app/src/main/java/com/mc/custom/hencoder/practie002/Practice002ComposeShaderView.java
@@ -1,13 +1,23 @@
 package com.mc.custom.hencoder.practie002;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ComposeShader;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-/**
+import com.mc.custom.R;
+
+/**ComposeShader 混合着色器 所谓混合，就是把两个 Shader 一起使用
  * @author mc
  * @darte 2018/9/19
  * @email zzzcy1014@gmail.com
@@ -26,6 +36,18 @@ public class Practice002ComposeShaderView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        // 第一个 Shader：头像的 Bitmap
+        Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+        Shader shader1 = new BitmapShader(bitmap1, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
 
+
+        Shader shader2 = new RadialGradient(300, 300, 200, Color.parseColor("#E91E63"),
+                Color.parseColor("#2196F3"), Shader.TileMode.MIRROR);
+
+        // ComposeShader：结合两个 Shader
+        //Shader shader = new ComposeShader(shader1, shader2, PorterDuff.Mode.SRC_OVER);
+        paint.setShader(shader1);
+
+        canvas.drawCircle(300, 300, 300, paint);
     }
 }
