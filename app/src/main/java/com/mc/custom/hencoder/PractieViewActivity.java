b475e4b945ac2cc3ebc5801ab32f7fcdf2a58cec
MilletCongee <zzzcy1014@aliyun.com>
2018-09-21 18:16:53 +0800

    Practie004Activity

--- app/src/main/java/com/mc/custom/hencoder/PractieViewActivity.java
+++ app/src/main/java/com/mc/custom/hencoder/PractieViewActivity.java
@@ -15,6 +15,8 @@ import com.mc.custom.confirm.Confirm003Activity;
 import com.mc.custom.confirm.ConfirmActivity;
 import com.mc.custom.hencoder.practie001.Practie001Activity;
 import com.mc.custom.hencoder.practie002.Practie002Activity;
+import com.mc.custom.hencoder.practie003.Practie003Activity;
+import com.mc.custom.hencoder.practie004.Practie004Activity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +27,7 @@ import java.util.List;
  * @email zzzcy1014@gmail.com
  * @since JDK1.8+
  */
+//自定义 View 最关键的有三个点：绘制、布局和触摸反馈
 public class PractieViewActivity extends BaseActivity {
     private RecyclerView mRecyclerView;
     private MilletCindeeAdapter mAdapter;
@@ -43,13 +46,13 @@ public class PractieViewActivity extends BaseActivity {
     @Override
     protected void initData(Bundle savedInstanceState) {
         mList = new ArrayList<String>();
-        mList.add("自定义 View 1-1 绘制基础");
-        mList.add("自定义 View 1-2 Paint 详解");
-        mList.add("自定义 View 1-3 drawText() 文字的绘制");
+        mList.add(" 绘制基础");
+        mList.add(" Paint 详解");
+        mList.add("drawText() 文字的绘制");
 
-        mList.add("4");
-        mList.add("5");
-        mList.add("6");
+        mList.add("围裁切 几何变换 Matrix graphics.Camera   ");
+        mList.add("绘制顺序");
+        mList.add("动画");
         //设置RecyclerView管理器
         mAdapter = new MilletCindeeAdapter(this, mList);
         //设置RecyclerView管理器
@@ -76,8 +79,10 @@ public class PractieViewActivity extends BaseActivity {
                         startActivity(new Intent(PractieViewActivity.this, Practie002Activity.class));
                         break;
                     case 2:
+                        startActivity(new Intent(PractieViewActivity.this, Practie003Activity.class));
                         break;
                     case 3:
+                        startActivity(new Intent(PractieViewActivity.this, Practie004Activity.class));
                         break;
                     default:
                         break;
---
+++ app/src/main/java/com/mc/custom/hencoder/practie004/Practice004RotateView.java
@@ -0,0 +1,34 @@
+package com.mc.custom.hencoder.practie004;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class Practice004RotateView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+
+    public Practice004RotateView(Context context) {
+        super(context);
+    }
+
+    public Practice004RotateView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice004RotateView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
